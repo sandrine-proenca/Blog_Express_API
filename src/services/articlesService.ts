811392d3e2fa344dbea3c1290bfa5009ff6c3020
PROENCA Sandrine (SNCF VOYAGEURS / ESV TGV Sud Ouest / BCC TOULOUSE) <7410499r@commun.ad.sncf.fr>
2023-01-17 11:03:12 +0100

    mise en TS de ArticlesService

--- src/services/articlesService.ts
+++ src/services/articlesService.ts
@@ -27,27 +27,27 @@ export class ArticlesService {
 
     // création d'un article
     async postArticles(chronicle: string): Promise <TArticle | undefined> {
-        const data = await client.query('INSERT INTO articleS (chronicle) VALUES ($1) RETURNING *',[chronicle]);
-        if (data.rowCount){
-            return data.rows[0];
+        const articles: QueryResult <TArticle> = await client.query('INSERT INTO articleS (chronicle) VALUES ($1) RETURNING *',[chronicle]);
+        if (articles.rowCount>0){
+            return articles.rows[0];
         }
         return undefined;
     };
 
     // modification d'un article
-    async putArticles(id: number, chronicle: string, userID: number){
-        const data = await client.query('UPDATE articleS SET chronicle=$2 WHERE id=$1 RETURNING *', [id, chronicle, userID]);
-        if (data.rowCount) {
-            return data.rows[0];
+    async putArticles(id: number, chronicle: string, userID: number): Promise <TArticle | undefined> {
+        const articles: QueryResult <TArticle> = await client.query('UPDATE articleS SET chronicle=$2 WHERE id=$1 RETURNING *', [id, chronicle, userID]);
+        if (articles.rowCount) {
+            return articles.rows[0];
         }
         return undefined
     };
 
     // suppression d'un article
-    async deleteArticles(id: number) {
-        const data = await client.query('DELETE FROM articles WHERE id=$1 RETURNING *', [id]);
-        if (data.rowCount) {
-            return data.rows[0];
+    async deleteArticles(id: number):Promise <TArticle | undefined> {
+        const articles: QueryResult <TArticle> = await client.query('DELETE FROM articles WHERE id=$1 RETURNING *', [id]);
+        if (articles.rowCount) {
+            return articles.rows[0];
         }
         return undefined;
     };
