ec6554ba14cdb1a75407e38c7e29288ace739057
PROENCA Sandrine (SNCF VOYAGEURS / ESV TGV Sud Ouest / BCC TOULOUSE) <7410499r@commun.ad.sncf.fr>
2023-01-18 15:26:52 +0100

    auth retiré sur les get dans articlesRouter

--- src/routes/articlesRouter.ts
+++ src/routes/articlesRouter.ts
@@ -9,9 +9,9 @@ export const articlesRouter = express.Router();
 const articlesController = new ArticlesController();
 
     // récupération de tous les articles
-    
-    articlesRouter.get('/', authenticateJWT, articlesController.getAllArticles);
-    articlesRouter.get('/:id', authenticateJWT, articlesController.getArticlesById);
+
+    articlesRouter.get('/', articlesController.getAllArticles);
+    articlesRouter.get('/:id', articlesController.getArticlesById);
     articlesRouter.post('/', authenticateJWT, articlesController.postArticles);
     articlesRouter.put('/', authenticateJWT, articlesController.putArticles);
     articlesRouter.delete('/:id',authenticateJWT, articlesController.deleteArticles);
