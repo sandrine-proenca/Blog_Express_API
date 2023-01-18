ccd2ba41e39054002f2a0f0ff79b2083deaddc9c
PROENCA Sandrine (SNCF VOYAGEURS / ESV TGV Sud Ouest / BCC TOULOUSE) <7410499r@commun.ad.sncf.fr>
2023-01-18 09:29:37 +0100

    changement de la table

--- sql/blog_express_ pgAdmin4.sql
+++ sql/blog_express_ pgAdmin4.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS public.articles
 (
     id serial NOT NULL,
     chronicle character varying COLLATE pg_catalog."default" NOT NULL,
-    created_date date,
+    created_date date DEFAULT CURRENT_DATE,
     deleted_date date,
     user_id integer NOT NULL,
     CONSTRAINT articles_pkey PRIMARY KEY (id)
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS public.comments
 (
     id serial NOT NULL,
     message character varying COLLATE pg_catalog."default",
-    created_date date,
+    created_date date DEFAULT CURRENT_DATE,
     deleted_date date,
     article_id integer NOT NULL,
     user_id integer NOT NULL,
@@ -35,7 +35,6 @@ CREATE TABLE IF NOT EXISTS public.users
     id serial NOT NULL,
     name character varying COLLATE pg_catalog."default" NOT NULL,
     password character varying COLLATE pg_catalog."default" NOT NULL,
-    admin boolean NOT NULL,
     CONSTRAINT users_pkey PRIMARY KEY (id)
 );
 
