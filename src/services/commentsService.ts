ff0e26a93daac301e27953f037c5873f6d9595cd
FAVRE Jessie (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC HOURCADE GLA) <8009383b@commun.ad.sncf.fr>
2023-01-19 16:57:34 +0100

    derniers fichiers commentés

--- src/services/commentsService.ts
+++ src/services/commentsService.ts
@@ -1,10 +1,11 @@
+// Les imports:
 import { client } from '../client'
 import TComments from '../types/TComments'
 import { QueryResult } from "pg";
 
 export class CommentsService {
 
-    //récupération de tous les commentaires d'un article:
+    // récupération de tous les commentaires d'un article:
     async getAllCommentsByArticleId(articleId: number): Promise<TComments[] | undefined> {
         const comments: QueryResult<TComments> = await client.query('SELECT * FROM comments WHERE article_id = $1', [articleId]);
         console.log(comments, articleId);
@@ -16,7 +17,7 @@ export class CommentsService {
         return undefined;
     }
 
-    //récupération d'un seul commentaire d'un article:
+    // récupération d'un seul commentaire d'un article:
     async getOneCommentByArticleId(commentId: number, articleId: number): Promise <TComments[] | undefined>{
         const comments: QueryResult<TComments> = await client.query('SELECT * FROM comments WHERE id=$1 AND article_id=$2 RETURNING *', [commentId, articleId]);
         if (comments.rowCount){
@@ -24,7 +25,7 @@ export class CommentsService {
         }
         return undefined;
     }
-
+// récupération d'un commentaire par son id:
     async getCommentById(id: number): Promise<TComments | undefined> {
         const data: QueryResult<TComments> = await client.query('SELECT * FROM comments WHERE id=$1', [id])
         if (data.rowCount) {
@@ -33,7 +34,7 @@ export class CommentsService {
         return undefined
     }
 
-    //création d'un commentaire dans un article:
+    // création d'un commentaire dans un article:
     async postComment(message: string, articleId:number, userId: number): Promise<TComments | undefined> {
         const comments: QueryResult<TComments> = await client.query('INSERT INTO comments (message, article_id, user_id) VALUES ($1, $2, $3) RETURNING *', [message, articleId, userId]);
         if (comments.rowCount) {
@@ -42,14 +43,14 @@ export class CommentsService {
         return undefined;
     }
 
-    //modification d'un commentaire dans un article:
+    // modification d'un commentaire:
     async putComment(id: number, message: string): Promise<TComments | undefined> {
         const comments: QueryResult<TComments> = await client.query('UPDATE comments SET message=$1 WHERE id=$2 RETURNING *', [message, id]);
         if (comments.rowCount)
             return comments.rows[0];
     }
 
-    //Suppression d'un commentaire dans un article:
+    // Suppression d'un commentaire:
     async deleteCommentById(id: number): Promise<TComments | undefined> {
         const comments: QueryResult<TComments> = await client.query('DELETE FROM comments WHERE id=$1 RETURNING *', [id]);
         if (comments.rowCount)
